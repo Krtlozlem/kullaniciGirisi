0328e9439f60359a86afbdc5ca1d62fba7d00dd6
ozlem kartal <ozlmkrtl1@gmail.com>
2023-05-31 12:56:39 +0300

    kullanici girisi yapildi

---
+++ kullaniciGirisi.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class kullaniciGirisi {
+    public static void main(String[] args) {
+        String userName, passwords;
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Kullanıcı Girişi: ");
+        userName = input.nextLine();
+
+        System.out.print("Şifreniz: ");
+        passwords = input.nextLine();
+
+        // line burada string metotlar için kullanılan terim.
+
+        if(userName.equals("patika") && passwords.equals("java123")){
+            System.out.println("Girişiniz Başarılı :) ");
+
+        }else{
+            System.out.println("Başarısız Giriş Yaptınız! Tekrar Deneyin");
+        }
+    }
+}
